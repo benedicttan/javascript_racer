bd018987ad164027661e9445a2b8e573b3bed119
Code Division <codedivision@11th.ventures>
2014-12-03 10:19:59 +0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(document).ready(function() {
+  $(document).on('keyup', function(event) {
+    var key = event.which;
+    if (key == 81) { // Q was pressed
+      $("#player1_strip td.active").next().addClass("active");
+    }
+    else if (key == 80) { // P was pressed
+      $("#player2_strip td.active").next().addClass("active");
+    }
+    $("td.active").prev().removeClass("active");
+
+    if ($("#player1_strip td").last().hasClass("active") || $("#player2_strip td").last().hasClass("active")) {
+      alert("Game ended");
+    }
+    // Detect which key was pressed and call the appropriate function
+    // Google "jquery keyup what key was pressed" if you don't know how
+  });
+
+  $( "#restart" ).click(function() {
+    $("td.active").removeClass("active");
+    $("#player1_strip td").first().addClass("active");
+    $("#player2_strip td").first().addClass("active");
+  });
+
+});
